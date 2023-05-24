a13559637b966269c53b8b158a33dae431b2727e
mkoper02 <maciej.koperdowski@gmail.com>
2023-05-24 15:20:40 +0200

    Functions declaration

--- headers/BST.h
+++ headers/BST.h
@@ -1,7 +1,50 @@
+#include <vector>
+
+using namespace std;
+
+// Class representing node for BST
+class Node{
+public:
+    int value;          // value of thenode (key)
+    Node* parent;       // pointer to node's parent
+    Node* left;         // pointer to left child
+    Node* right;        // pointer to right child
+
+public:
+    // Default constructor
+    Node();
+
+    // Parameterized constructor
+    Node(int value, Node* parent);
+};
+
+// Class representing Binary Search Tree (BST)
 class BST {
 private:
-    /* data */
+    Node* root;         // root of the tree
+
 public:
-    BST(/* args */);
+    // Default constructor, create empty BST
+    BST();
+
+    // Constructor, create BST based on given data
+    BST(vector<int> elements);
+
+    // Destructor
     ~BST();
+
+    // Add given element to the tree
+    void add(int value);
+
+    // Remove given element from the tree
+    void remove(int value);
+
+    // Search for element with given value and return its address
+    Node* find(int value);
+
+    // Print BST
+    void print();
+
+    // Root getter
+    Node* getRoot() { return root; }
 };
--- src/DataStructures/BST.cpp
+++ src/DataStructures/BST.cpp
@@ -1,9 +1,42 @@
 #include "BST.h"
 
-BST::BST(/* args */) {
+Node::Node() {
+    value = 0;
+    parent = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
 
+Node::Node(int value, Node *parent) {
+    this->value;
+    this->parent = parent;
+    left = nullptr;
+    right = nullptr;
+}
+
+BST::BST() {
+    root = nullptr;
+}
+
+BST::BST(vector<int> elements)
+{
 }
 
 BST::~BST() {
     
+}
+
+void BST::add(int value) {
+
+}
+
+void BST::remove(int value) {
+}
+
+Node *BST::find(int value) {
+    return nullptr;
+}
+
+void BST::print() {
+
 }
