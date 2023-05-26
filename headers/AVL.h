1801f8702af57ed6d08c5cb9d42a7573dc79af46
mkoper02 <maciej.koperdowski@gmail.com>
2023-05-26 16:24:54 +0200

    AVL first functions

--- headers/AVL.h
+++ headers/AVL.h
@@ -1,7 +1,33 @@
+// Class representing node for AVL tree
+class Node {
+public:
+    int value;          // value of the node
+    Node* left;         // pointer to left child
+    Node* right;        // pointer to right child
+
+public:
+    // Default constructor
+    Node();
+
+    // Parameterized constructor
+    Node(int value);
+};
+
+// Class representing AVL tree
 class AVL {
 private:
-    /* data */
+    Node* root;         // root of the tree
+
 public:
-    AVL(/* args */);
+    // Default constructor, create empty AVL
+    AVL();
+
+    // Destructor
     ~AVL();
+
+    // Get height of the given node
+    int getHeight(Node* node);
+
+    // Get balance factor of the given node
+    int getBalanceFactor(Node* node);
 };
--- src/DataStructures/AVL.cpp
+++ src/DataStructures/AVL.cpp
@@ -1,9 +1,34 @@
 #include "AVL.h"
 
-AVL::AVL(/* args */) {
-    
+Node::Node() {
+    value = 0;
+    left = nullptr;
+    right = nullptr;
+}
+
+Node::Node(int value) {
+    this->value = value;
+    left = nullptr;
+    right = nullptr;
+}
+
+AVL::AVL() {
+    root = nullptr;
 }
 
 AVL::~AVL() {
 
+}
+
+int AVL::getHeight(Node *node) {
+    // If tree is empty
+    if (node == nullptr) return -1;
+
+    // Check height of the subtrees
+    int left_height = getHeight(node->left);
+    int right_height = getHeight(node->right);
+
+    // choose larger value
+    if (left_height > right_height) return left_height + 1;
+    else return right_height + 1;
 }
