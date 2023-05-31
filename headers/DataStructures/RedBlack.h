75af07661a4c7914fae51fc6777926d410167b0f
mkoper02 <maciej.koperdowski@gmail.com>
2023-06-01 00:44:26 +0200

    Working add and remove functions

--- headers/DataStructures/RedBlack.h
+++ headers/DataStructures/RedBlack.h
@@ -17,22 +17,30 @@ public:
 
     // Remove element with given value from the tree
     void remove(int value);
+    
+    // Print tree and add letter R next to red nodes
+    void print() override;
 
+private:
     // Balance tree and fix colours after adding new node
-    void balanceTreeAdd(Node_RB* main_node, Node_RB* new_node);
+    // void balanceTreeAdd(Node_RB* main_node, Node_RB* new_node);
 
     // Balance tree and fix colours after removing node
-    void balanceTreeRemove(Node_RB* node);
+    // void balanceTreeRemove(Node_RB* node);
 
     // Rotation of the tree
+    void rotate(Node_RB* node);
     void leftRotation(Node_RB* node);
     void rightRotation(Node_RB* node);
     void leftRightRotation(Node_RB* node);
     void rightLeftRotation(Node_RB* node);
 
-    // Change colours where necassary
-    void fixColours();
+    // Change colours and balance tree when necessary
+    void checkColour(Node_RB* node);
 
-    // Print tree and add letter R next to red nodes
-    void print() override;
+    // Balance tree
+    void correctTree(Node_RB* node);
+
+    // Check if node is left child
+    bool isLeftChild(Node_RB* node);
 };
